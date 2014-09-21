153279728af3b21dce92f81ef1a97aabbfa9d407
Daniel Wieland <d.wieland@arcor.de>
2014-09-21 23:54:52 +0200

    Changed pom.xml, added log4j2 and create a main class

---
+++ src/main/java/jdw/xjcp/client/Client.java
@@ -0,0 +1,19 @@
+package jdw.xjcp.client;
+
+/**
+ * A simple XJCP based client.
+ * 
+ * @author TheDwoon
+ * @date 21.09.2014
+ * @version 1.0.0
+ *
+ */
+public final class Client {
+	private Client() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
