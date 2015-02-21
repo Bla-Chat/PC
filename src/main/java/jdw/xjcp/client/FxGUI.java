3c19d07582a82beb8cb95283e014548a7ba8ed80
Sigmar Walter <sigmar-walter@web.de>
2015-02-21 01:53:02 +0100

    Fixed dark theme. Fixed pom setup.

--- src/main/java/jdw/xjcp/client/FxGUI.java
+++ src/main/java/jdw/xjcp/client/FxGUI.java
@@ -3,7 +3,6 @@ package jdw.xjcp.client;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -26,92 +25,91 @@ import net.michaelfuerst.xjcp.protocol.ChatMessage;
 import net.michaelfuerst.xjcp.protocol.Contact;
 import net.michaelfuerst.xjcp.protocol.Conversation;
 
-
 public final class FxGUI extends Application {
 	public static final URL MAIN_WINDOW_LOC = FxGUI.class.getResource("/fxml/mainWindow.fxml");
 
 	private String host;
 	private String user;
-	
+
 	private XJCP xjcp;
 	private Pane page;
-	
+
 	@FXML
 	private TabPane chatPane;
-	
+
 	@FXML
 	private VBox userPane;
-	
+
 	public FxGUI() throws IOException {
-		
+
 	}
-	
-	public static void main(final String[] args) throws InterruptedException, ExecutionException, TimeoutException, IOException {
-//		System.out.println(user + "@" + host + " using " + password);
-//		
-//		XJCP xjcp = new DesktopXJCP(host);
-//		
-//		System.out.println("login: " + xjcp.login(user, password).get(2, TimeUnit.SECONDS));
-//		System.out.println("contacts: " + xjcp.requestContacts().get(2, TimeUnit.SECONDS));
-//		System.out.println("chats: " + xjcp.requestChats().get(2, TimeUnit.SECONDS));
-//		System.out.println("messages: " + xjcp.requestHistory("penguinmenac3,thedwoon", 2).get(2, TimeUnit.SECONDS));
-//
-//		xjcp.shutdown();
-		
+
+	public static void main(final String[] args) throws InterruptedException, ExecutionException,
+		TimeoutException, IOException {
+		// System.out.println(user + "@" + host + " using " + password);
+		//
+		// XJCP xjcp = new DesktopXJCP(host);
+		//
+		// System.out.println("login: " + xjcp.login(user, password).get(2, TimeUnit.SECONDS));
+		// System.out.println("contacts: " + xjcp.requestContacts().get(2, TimeUnit.SECONDS));
+		// System.out.println("chats: " + xjcp.requestChats().get(2, TimeUnit.SECONDS));
+		// System.out.println("messages: " + xjcp.requestHistory("penguinmenac3,thedwoon", 2).get(2,
+		// TimeUnit.SECONDS));
+		//
+		// xjcp.shutdown();
+
 		launch(args);
 	}
-	
+
 	@Override
 	public void init() throws Exception {
 		host = getParameters().getRaw().get(0);
 		user = getParameters().getRaw().get(1);
-		
+
 		xjcp = new DesktopXJCP(host);
 		xjcp.login(user, getParameters().getRaw().get(2)).get(5, TimeUnit.SECONDS);
-		
+
 		FXMLLoader loader = new FXMLLoader(MAIN_WINDOW_LOC);
 		loader.setController(this);
 		page = (Pane) loader.load();
 	}
-	
+
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
-		Random random = new Random();
-
 		List<Conversation> conversations = xjcp.requestChats().get();
 		for (Conversation c : conversations) {
 			Chat chat = new Chat(c.getName());
-			
+
 			ChatHistory history = xjcp.requestHistory(c.getConversation(), 100).get();
 			for (ChatMessage m : history.getMessages()) {
 				MessageType type = MessageType.ME;
 				if (!m.getNick().equalsIgnoreCase(user)) {
 					type = MessageType.OTHER;
 				}
-				
+
 				chat.addMessages(new Message(type, m.getText()));
 			}
-			
+
 			chatPane.getTabs().add(chat);
 		}
-	
+
 		List<Contact> contacts = xjcp.requestContacts().get();
 		for (Contact c : contacts) {
-			User user = new User(c.getNick(), this.user + "@" + this.host);
+			User user = new User(c.getNick(), this.user + "@" + host);
 			if (c.getStatus() != 0) {
 				user.setOnlineState(UserState.ONLINE);
 			} else {
 				user.setOnlineState(UserState.OFFLINE);
 			}
-			
+
 			userPane.getChildren().add(user.getRootPane());
 		}
-		
+
 		userPane.getChildren().setAll(userPane.getChildren().sorted());
-		
+
 		Scene scene = new Scene(page);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("Bla Chat");		
+		primaryStage.setTitle("Bla Chat");
 		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icon/icon.png")));
 		primaryStage.show();
 	}
