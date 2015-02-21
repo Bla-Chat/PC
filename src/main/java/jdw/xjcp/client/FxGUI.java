5a9600cc6bc40f81cf6a4c1bf03ae142b809dd1d
Daniel Wieland <d.wieland@arcor.de>
2015-02-21 01:24:12 +0100

    somehow it works

--- src/main/java/jdw/xjcp/client/FxGUI.java
+++ src/main/java/jdw/xjcp/client/FxGUI.java
@@ -2,7 +2,11 @@ package jdw.xjcp.client;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javafx.application.Application;
 import javafx.fxml.FXML;
@@ -14,11 +18,22 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import jdw.xjcp.client.Message.MessageType;
+import jdw.xjcp.client.User.UserState;
+import net.michaelfuerst.xjcp.DesktopXJCP;
+import net.michaelfuerst.xjcp.XJCP;
+import net.michaelfuerst.xjcp.protocol.ChatHistory;
+import net.michaelfuerst.xjcp.protocol.ChatMessage;
+import net.michaelfuerst.xjcp.protocol.Contact;
+import net.michaelfuerst.xjcp.protocol.Conversation;
 
 
 public final class FxGUI extends Application {
 	public static final URL MAIN_WINDOW_LOC = FxGUI.class.getResource("/fxml/mainWindow.fxml");
 
+	private String host;
+	private String user;
+	
+	private XJCP xjcp;
 	private Pane page;
 	
 	@FXML
@@ -31,36 +46,68 @@ public final class FxGUI extends Application {
 		
 	}
 	
-	public static void startGUI() {
-		launch();
+	public static void main(final String[] args) throws InterruptedException, ExecutionException, TimeoutException, IOException {
+//		System.out.println(user + "@" + host + " using " + password);
+//		
+//		XJCP xjcp = new DesktopXJCP(host);
+//		
+//		System.out.println("login: " + xjcp.login(user, password).get(2, TimeUnit.SECONDS));
+//		System.out.println("contacts: " + xjcp.requestContacts().get(2, TimeUnit.SECONDS));
+//		System.out.println("chats: " + xjcp.requestChats().get(2, TimeUnit.SECONDS));
+//		System.out.println("messages: " + xjcp.requestHistory("penguinmenac3,thedwoon", 2).get(2, TimeUnit.SECONDS));
+//
+//		xjcp.shutdown();
+		
+		launch(args);
 	}
 	
 	@Override
 	public void init() throws Exception {
+		host = getParameters().getRaw().get(0);
+		user = getParameters().getRaw().get(1);
+		
+		xjcp = new DesktopXJCP(host);
+		xjcp.login(user, getParameters().getRaw().get(2)).get(5, TimeUnit.SECONDS);
+		
 		FXMLLoader loader = new FXMLLoader(MAIN_WINDOW_LOC);
 		loader.setController(this);
-		page = (Pane) loader.load();	
+		page = (Pane) loader.load();
 	}
 	
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
 		Random random = new Random();
-		// FIXME fix me
-		for (int i = 0; i < 3; i++) {
-			Chat chat = new Chat("Test" + i);
-			for (int j = 0; j < 100; j++) {
-				chat.addMessages(new Message(random.nextBoolean() ? MessageType.ME : MessageType.OTHER, Long.toHexString(random.nextLong())));
+
+		List<Conversation> conversations = xjcp.requestChats().get();
+		for (Conversation c : conversations) {
+			Chat chat = new Chat(c.getName());
+			
+			ChatHistory history = xjcp.requestHistory(c.getConversation(), 100).get();
+			for (ChatMessage m : history.getMessages()) {
+				MessageType type = MessageType.ME;
+				if (!m.getNick().equalsIgnoreCase(user)) {
+					type = MessageType.OTHER;
+				}
+				
+				chat.addMessages(new Message(type, m.getText()));
 			}
 			
 			chatPane.getTabs().add(chat);
-		}		
+		}
+	
+		List<Contact> contacts = xjcp.requestContacts().get();
+		for (Contact c : contacts) {
+			User user = new User(c.getNick(), this.user + "@" + this.host);
+			if (c.getStatus() != 0) {
+				user.setOnlineState(UserState.ONLINE);
+			} else {
+				user.setOnlineState(UserState.OFFLINE);
+			}
+			
+			userPane.getChildren().add(user.getRootPane());
+		}
 		
-		userPane.getChildren().add(new User("1", uselessDesc()).getRootPane());
-		userPane.getChildren().add(new User("2", uselessDesc()).getRootPane());
-		userPane.getChildren().add(new User("3", uselessDesc()).getRootPane());
-		userPane.getChildren().add(new User("4", uselessDesc()).getRootPane());
-		userPane.getChildren().add(new User("5", uselessDesc()).getRootPane());
-		userPane.getChildren().add(new User("6", uselessDesc()).getRootPane());
+		userPane.getChildren().setAll(userPane.getChildren().sorted());
 		
 		Scene scene = new Scene(page);
 		primaryStage.setScene(scene);
@@ -69,7 +116,8 @@ public final class FxGUI extends Application {
 		primaryStage.show();
 	}
 
-	private String uselessDesc() {
-		return Long.toHexString(new Random().nextLong());
+	@Override
+	public void stop() throws Exception {
+		xjcp.shutdown();
 	}
 }
--- src/main/java/jdw/xjcp/client/User.java
+++ src/main/java/jdw/xjcp/client/User.java
@@ -9,7 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
-public final class User {
+public final class User implements Comparable<User> {
 	private static final URL FXML_SOURCE = User.class.getResource("/fxml/user.fxml");
 	
 	private Pane rootPane;
@@ -19,7 +19,14 @@ public final class User {
 	@FXML
 	private Label name;
 	
+	private UserState state;
+	
+
 	public User(final String name, final String info) throws IOException {
+		this(name, info, UserState.OFFLINE);
+	}
+	
+	public User(final String name, final String info, final UserState state) throws IOException {
 		Objects.requireNonNull(name);
 		Objects.requireNonNull(info);
 		
@@ -29,9 +36,42 @@ public final class User {
 		
 		this.info.setText(info);
 		this.name.setText(name);
+		
+		setOnlineState(state);
+	}
+	
+	public void setOnlineState(final UserState state) {
+		rootPane.getStyleClass().clear();
+		rootPane.getStyleClass().add(state.getNodeStyleClass());
+		this.state = state;
 	}
 	
 	public Pane getRootPane() {
 		return rootPane;
+	}
+	
+	public static enum UserState {
+		ONLINE("user-online"), OFFLINE("user-offline");
+
+        private final String nodeStyleClass;
+
+        private UserState(final String nodeStyleClass) {
+            this.nodeStyleClass = nodeStyleClass;
+        }
+
+        public String getNodeStyleClass() {
+            return nodeStyleClass;
+        }
+	}
+	
+	@Override
+	public int compareTo(final User o) {
+		int difference = o.state.ordinal() - state.ordinal();
+		
+		if (difference == 0) {
+			difference = name.getText().compareTo(o.name.getText());
+		}
+		
+		return difference;
 	}
 }
